cad72db80a7dc98802f6726f7c0dbaa5d5cbade2
cxdeng <chengx.deng"icloud.com>
2023-07-02 01:56:17 +0100

    stat function

---
+++ property/01_stat.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    printf("argc = %d, argv[0] = %s, argv[1] = %s\n", argc, argv[0], argv[1]);
+    if (argc != 2)
+    {
+        printf("Invalid arguments\n");
+        exit(-1);
+    }
+    
+
+    struct stat *sta = (struct stat *)malloc(sizeof(struct stat));
+    int res = stat(argv[1], sta);
+    if (res == -1)
+    {
+       perror("stat fail");
+       _exit(-1);
+    }
+
+    printf("file name = %s\n", argv[1]);
+    printf("st_uid = %d\n", sta->st_uid);
+    printf("st_gid = %d\n", sta->st_gid);
+    printf("st_mode = %u\n", sta->st_mode);
+    printf("st_nlink = %ld\n", sta->st_nlink);
+    printf("st_uid = %d\n", sta->st_uid);
+    printf("st_size = %ld\n", sta->st_size);
+
+
+    return 0;
+}
